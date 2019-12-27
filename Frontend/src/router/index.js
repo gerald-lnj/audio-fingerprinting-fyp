704b6e49ec6c74f2876e8e06e02a1dfe9e17fc8a
Gerald <gerald.lau.95@gmail.com>
2019-12-27 10:26:15 +0800

    added vue basic app

---
+++ Frontend/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Ping from '../components/Ping.vue';
+import NotFound from '../components/NotFound.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/ping',
+    name: 'Ping',
+    component: Ping,
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
