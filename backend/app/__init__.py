227e168d43770a91618f1bd9e73b47efc4b6ba3f
Gerald <gerald.lau.95@gmail.com>
2019-12-29 11:00:37 +0800

    added user controllers

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,40 @@
 from flask import Flask
 from flask_cors import CORS
+import os
+import json
+import datetime
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+load_dotenv()
+
+# extend json-encoder class to support ObjectId & datetime data types
+# used to store ‘_id’ & ‘time-stamp’ respectively in MongoDB, convert to strings
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 app = Flask(__name__)
 
 # configuration
 DEBUG = True
 
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
+# add mongo url to flask config, so that flask_pymongo can use it to make connection
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
+# use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
+app.json_encoder = JSONEncoder
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 from app import routes
+from app.controllers import *
--- backend/backend.py
+++ backend/backend.py
@@ -1,1 +1,3 @@
 from app import app
+
+# entrypoint to flask app
