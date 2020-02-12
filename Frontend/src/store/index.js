8ea82b693f1d3fb725f54f46fa76edc92702e2cd
Gerald <gerald.lau.95@gmail.com>
2020-02-13 00:03:28 +0800

    added vuex store

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -5,11 +5,39 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    loggedIn: false,
+    name: '',
+    email: '',
+    refresh: '',
+    jwt: '',
   },
   mutations: {
+    updateDetails(state, resp) {
+      state.email=resp.data.data.email
+      state.name=resp.data.data.name
+      state.refresh=resp.data.data.refresh
+      state.jwt=resp.data.data.token
+    },
+    updateLoginStatus(state) {
+      state.loggedIn = state.email.length > 0
+    },
+    resetState(state) {
+      Object.assign(state, getDefaultState())
+      console.log(state)
+    }
   },
   actions: {
   },
   modules: {
   },
 });
+
+const getDefaultState= () => {
+  return {
+    loggedIn: false,
+    name: '',
+    email: '',
+    refresh: '',
+    jwt: '',
+  }
+}
