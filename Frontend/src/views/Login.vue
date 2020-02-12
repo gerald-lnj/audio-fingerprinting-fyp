7b34123b292086b1e7d0cdab867dce77ff73f607
Gerald <gerald.lau.95@gmail.com>
2020-02-13 00:04:04 +0800

    added login page

---
+++ Frontend/src/views/Login.vue
@@ -0,0 +1,148 @@
+<template>
+  <v-row>
+    <v-col align="center">
+      <v-col>
+        <v-text-field
+          v-model="email"
+          :rules="[rules.required, rules.email]"
+          label="Email"
+        />
+      </v-col>
+
+      <v-col>
+        <v-text-field
+          v-model="password"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required]"
+          :type="showPassword ? 'text' : 'password'"
+          name="input-10-1"
+          label="Password"
+          hint="At least 8 characters"
+          counter
+          @click:append="showPassword = !showPassword"
+        />
+      </v-col>
+
+      <v-col>
+        <v-btn
+          class="button mt-3 mv-5"
+          variant="warning" 
+          pill 
+          @click="login"
+        >
+          Login
+        </v-btn>
+      </v-col>
+      <v-col>
+        <p class="mt-3 mv-0 text-white">
+          No Account?
+          <br>
+        </p>
+        <v-btn
+          to="/sign-up"
+        >
+          Sign Up Here
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn
+          @click="onPasswordReset"
+        >
+          Forgot your password?
+        </v-btn>
+      </v-col>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Axios from "axios";
+export default {
+
+  name: "Login",
+  data: () => ({
+    email: "",
+    password: "",
+    showPassword: false,
+    server_url: process.env.VUE_APP_SERVER_URL,
+    rules: {
+      required (value) {
+        return !!value||'Required'
+      },
+      email (value) {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return pattern.test(value) || 'Invalid e-mail.'
+      },
+    },
+
+  }),
+  methods: {
+    login() {
+      // const data = {
+      //   "email": this.email,
+      //   "password": this.password
+      // }
+      const data = {
+        "email": 'test@test.com',
+        "password": '12345'
+      }
+      Axios.post(`${this.server_url}/auth`, data)
+      .then((resp)=> {
+        this.$store.commit('updateDetails', resp)
+        this.$store.commit('updateLoginStatus')
+        this.$router.push('/')
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+
+
+
+    },
+    onPasswordReset() {
+      // tbc
+    },
+
+    onSubmitResetPassword() {
+      // tbc
+    }
+
+
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  font-family: "Autumn", "Avenir", Helvetica, Arial, sans-serif;
+  letter-spacing: 5px;
+  font-size: 44px;
+  color: white;
+}
+.login {
+  margin-top: 40px;
+}
+.input {
+  width: 75%;
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+button {
+  cursor: pointer;
+}
+p a {
+  text-decoration: underline;
+  color: #fffb11;
+  cursor: pointer;
+}
+.alert-style {
+  min-width: 200px;
+}
+/* Inserting this collapsed row between two flex items will make
+* the flex item that comes after it break to a new row */
+.break {
+  flex-basis: 100%;
+  height: 0;
+}
+
+</style>
