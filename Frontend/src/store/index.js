6d9ecb41b3542c38250f498f369f4c09a50b28df
Gerald <gerald.lau.95@gmail.com>
2020-02-13 16:43:39 +0800

    added plugin to allow vuex state to persist across refreshes

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from "vuex-persistedstate";
+
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
     loggedIn: false,
     name: '',
