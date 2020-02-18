871ad4205a1d424af393b8a7a065bf590e705ad0
Gerald <gerald.lau.95@gmail.com>
2020-02-18 16:24:21 +0800

    Modified to record audio

--- Frontend/src/views/Detect.vue
+++ Frontend/src/views/Detect.vue
@@ -1,103 +1,127 @@
 <template>
   <v-container>
-    <div class="videoRec text-xs-center">
-      <input 
-        ref="video_h" 
-        v-model="videoModel"
-        type="hidden" 
-        name="video" 
-      >
-      <video
-        ref="video"
-        class="video"
-        controls
-      />
-      <div class="video-controllers" />
-      <v-btn @click="startRecording('video1')">
-        Start Recording
-      </v-btn>
-      <v-btn @click="stopRecording('video1')">
-        Pause
-      </v-btn>
-      <v-btn @click="download('video1')">
-        Download
-      </v-btn>
-    </div>
+    <v-row>
+      <v-col align="center">
+        <v-col>
+          <audio
+            ref="audio"
+            controls
+            autoplay
+            playsinline
+          />
+        </v-col>
+        <v-col>
+          <transition name="mic">
+            <v-btn
+              :class="{'mic': recording}"
+              x-large
+              color="primary"
+              fab
+              text 
+              icon
+              @click="toggleRecording"
+            >
+              <v-icon>{{ recordingIcon }}</v-icon>
+            </v-btn>
+          </transition>
+        </v-col>
+
+        <v-col>
+          <v-btn @click="download">
+            Download (DEBUG)
+          </v-btn>
+        </v-col>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
+
+
 <script>
-let RecordRTC = require('recordrtc');
+const RecordRTC = require('recordrtc');
 export default {
-  name: 'Video',
+  name: 'Detect',
   data: function () {
     return {
-      videoModel:""
+      recording: false,
+      recordingIcon: "mdi-microphone"
     }
   },
-  mounted: function () {
-    let video = this.$refs.video;
-    video.muted = false;
-    video.controls = true;
-    video.autoplay = false;
-  },
   methods: {
-    successCallback(stream) {
-      var options = {
-        mimeType: 'video/webm;codecs=vp9', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
-        audioBitsPerSecond: 128000,
-        videoBitsPerSecond: 128000,
-        timeSlice:2000,
-        bitsPerSecond: 128000 // if this line is provided, skip above two
+    recordRTCOptns() {
+      const options = {
+        type: 'audio',
+        mimeType: 'audio/wav',
+        recorderType: RecordRTC.StereoAudioRecorder,
+        numberOfAudioChannels: 1,
+        desiredSampRate: 44100
       };
+      const isEdge = navigator.userAgent.indexOf('Edge') !== -1 && (!!navigator.msSaveOrOpenBlob || !!navigator.msSaveBlob);
+      const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+      console.log(`isEdge: ${isEdge}`)
+      console.log(`isSafari: ${isSafari}`)
+      if (isSafari||isEdge) {
+        options.bufferSize = 4096;
+        options.numberOfAudioChannels = 2;
+      }
+
+      return options
+    },
+    toggleRecording() {
+      if (this.recording) {
+        this.recording = false
+        this.recordingIcon = "mdi-microphone"
+        this.stopRecording()
+      } else {
+        this.recording = true
+        // this.recordingIcon = "mdi-stop"
+        this.startRecording()
+      }
+    },
+    successCallback(stream) {
+      const options = this.recordRTCOptns()
       this.stream = stream;
       this.recordRTC = RecordRTC(stream, options);
       this.recordRTC.startRecording();
-      // let video = this.$refs.video;
-      // video.src = window.URL.createObjectURL(stream);
-      //this.toggleControls();
     },
     errorCallback() {
       //handle error here
     },
-    processVideo(audioVideoWebMURL) {
-      let video = this.$refs.video;
-      // let recordRTC = this.recordRTC;
-      video.src = audioVideoWebMURL;
-      //this.toggleControls();
-      // var recordedBlob = recordRTC.getBlob();
-      // recordRTC.getDataURL(function (dataURL) { });
+    processAudio(audioURL) {
+      const audio = this.$refs.audio;
+      audio.src = audioURL;
     },
     startRecording() {
-      this.poster="";
-      let mediaConstraints = {
-      video: {
-        mandatory: {
-        minWidth: 700,
-        minHeight: 720
-        }
-      }, audio: true
-      };
+      const mediaConstraints = { audio: true };
       navigator.mediaDevices
       .getUserMedia(mediaConstraints)
       .then(this.successCallback.bind(this), this.errorCallback.bind(this));
     },
     stopRecording() {
-      this.poster="";
-      let recordRTC = this.recordRTC;
-      recordRTC.stopRecording(this.processVideo.bind(this));
-      let stream = this.stream;
+      const recordRTC = this.recordRTC;
+      recordRTC.stopRecording(this.processAudio.bind(this));
+      const stream = this.stream;
       stream.getAudioTracks().forEach(track => track.stop());
-      stream.getVideoTracks().forEach(track => track.stop());
     },
     download() {
-      this.recordRTC.save('video.webm');
+      this.recordRTC.save('audio.wav');
     }
   }
 }
 </script>
 
 <style scoped>
+@keyframes shadow-pulse
+{
+  0% {
+    box-shadow: 0 0 0 0px rgba(0, 0, 0, 0.2);
+  }
+  100% {
+    box-shadow: 0 0 0 20px rgba(0, 0, 0, 0);
+  }
+}
+
 .title {
   font-family: "Autumn", "Avenir", Helvetica, Arial, sans-serif;
   letter-spacing: 5px;
@@ -128,6 +152,10 @@ p a {
 .break {
   flex-basis: 100%;
   height: 0;
+}
+.mic
+{
+  animation: shadow-pulse 1s infinite;
 }
 
 </style>
